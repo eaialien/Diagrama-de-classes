558b0f1608a77b23df28c71d55df285173395bac
Vanessa Siqueira <eaialien0@gmail.com>
2024-07-18 14:19:40 -0300

    adicionar diagrama de classes uml e classes do iphone

---
+++ src/iPhone.java
@@ -0,0 +1,60 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música.");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz.");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    public static void main(String[] args) {
+        iPhone meuIphone = new iPhone();
+        meuIphone.tocar();
+        meuIphone.pausar();
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+        meuIphone.ligar("123456789");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+        meuIphone.exibirPagina("www.google.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
